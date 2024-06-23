7f3d22a7281022f79494b7d92ccf708b569abe1f
Ankit Vishwakarma <ankit99aug@outlook.com>
2024-06-23 12:19:04 +0530

    final commit

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://memories-wizard-0dc84d2639bf.herokuapp.com/' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
